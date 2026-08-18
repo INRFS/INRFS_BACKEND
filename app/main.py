63dc761017e62e66b9b17c80b80e697bac00fa22
Rajashekar157 <rajashekarmaddi562@gmail.com>
2026-08-18 19:42:22 +0530

    added settlement route

--- app/main.py
+++ app/main.py
@@ -7,28 +7,31 @@ from app.routes.investment_routes import router as investment_router
 from app.routes.investor_profile_routes import router as investor_profile_router
 from app.routes.investor_dashboard_routes import router as investor_dashboard_router
 
-from app.routes.admin.investor_management_route import (router as admin_investor_router,)
+from app.routes.admin.investor_management_route import (
+    router as admin_investor_router,
+)
 from app.routes.admin.investment_management_route import (
     router as admin_investment_router,
 )
 from app.routes.admin.admin_profile_route import (
     router as admin_profile_router,
 )
-from app.routes.admin.admin_dashboard_route import router as admin_dashboard_router
+from app.routes.admin.admin_dashboard_route import (
+    router as admin_dashboard_router,
+)
 from app.routes.admin.report_route import (
     router as admin_report_router,
 )
-from app.routes.admin.admin_tenure_timeout import router as admin_tenure_timeout_router
-
-from app.routes.investment_bond_route import router as report_router
-
+from app.routes.admin.admin_tenure_timeout import (
+    router as admin_tenure_timeout_router,
+)
 
 app = FastAPI(
     title="INRFS API",
     description="INRFS Investment Platform API",
     version="1.0.0",
+    root_path="/api",
 )
-
 
 app.add_middleware(
     CORSMiddleware,
@@ -36,12 +39,12 @@ app.add_middleware(
         "http://localhost:3000",
         "http://127.0.0.1:3000",
         "http://187.52.115.32",
+        "https://app.inrfs.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 app.include_router(auth_router)
 app.include_router(master_router)
@@ -55,6 +58,8 @@ app.include_router(admin_profile_router)
 app.include_router(admin_dashboard_router)
 app.include_router(admin_report_router)
 app.include_router(admin_tenure_timeout_router)
+
+
 @app.get("/")
 def root():
     return {
